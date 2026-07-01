ae92e820bb640e7dc9d013d376911dc8d7102787
yeutoanbilua001 <yeutoanbilua001@gmail.com>
2026-07-01 13:06:04 +0700

    Delete src directory

--- src/lib/helper/parseHTML.ts
+++
@@ -1,9 +0,0 @@
-export default function parseHTML(elem: string) {
-    let fragment = document.createDocumentFragment();
-    let newelement = document.createElement('div');
-    fragment.appendChild(newelement);
-    newelement.innerHTML = elem;
-    fragment.removeChild(fragment.firstChild);
-    fragment.appendChild(newelement.firstChild);
-    return fragment;
-}
